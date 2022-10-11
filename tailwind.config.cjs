5f3e26a294771dd25872a48909f952de9398aaf2
matiasfacello <matiasfacello@gmail.com>
2022-10-10 21:29:57 -0300

    added new fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
@@ -11,34 +11,11 @@ module.exports = {
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+				abel: ["'Abel'", ...defaultTheme.fontFamily.sans],
+				blinker: ["'Blinker'", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
-	darkMode: 'class',
+	plugins: [require("@tailwindcss/typography")],
+	darkMode: "class",
 };
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
